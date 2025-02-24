d5e688eb8c17b266ffd7728c21ff58a66ef84d62
sylwia <sylwiavv@gmail.com>
2025-02-24 13:57:38 +0100

    Create project structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,21 @@
+import { CalendarCheck } from 'lucide-react';
+import TaskList from '../features/tasks/components/TaskList';
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-
-
-   
+    <>
+      <main className="flex flex-col justify-center items-center min-h-screen gap-8">
+        <h1 className="flex flex-col items-center justify-center gap-4 mb-2">
+          <span className="p-3 rounded-full bg-font text-[#3e3341]">
+            <CalendarCheck />
+          </span>
+          To do list app
+        </h1>
+        <div className="flex flex-col gap-8 bg-[#3e3341] rounded-xl shadow-md min-w-[320px] p-6">
+          <TaskList />
+        </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-
-      </footer>
-    </div>
+      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center"></footer>
+    </>
   );
 }
