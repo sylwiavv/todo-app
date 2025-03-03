d6b1248cd0f94dd0af5d7b17d5a50ea955fe8751
sylwia <sylwiavv@gmail.com>
2025-03-04 00:04:15 +0100

    add dist folder

--- server/api/tasks/tasks.ts
+++ server/api/tasks/tasks.ts
@@ -4,14 +4,10 @@ import { VercelRequest, VercelResponse } from "@vercel/node";
 const prisma = new PrismaClient();
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
-  if (req.method === "GET") {
-    try {
-      const tasks = await prisma.task.findMany();
-      return res.status(200).json({ data: tasks });
-    } catch (error) {
-      return res.status(500).json({ error: "Internal Server Error" });
-    }
-  } else {
-    return res.status(405).json({ error: "Method Not Allowed" });
+  try {
+    const tasks = await prisma.task.findMany();
+    return res.status(200).json({ data: tasks });
+  } catch (error) {
+    return res.status(500).json({ error: "Internal Server Error" });
   }
 }
