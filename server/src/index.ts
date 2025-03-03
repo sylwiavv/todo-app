c27104fc357a776764e212d633fec6efe5916394
sylwia <sylwiavv@gmail.com>
2025-03-03 18:04:09 +0100

    install dependencies

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,12 @@
 const port = 9001; 
 import cors from "cors";
 import express from 'express';
+import path from 'path';
 
 const app = express();
 app.use(express.json());
 
+app.use(express.static(path.join(__dirname, "build")));
 
 // ------------------------------
 app.use("/tasks", require("./routes/taskRoute"));
